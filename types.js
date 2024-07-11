5a7ca10ea7df685787b26d0db5304a60b8c62b85
Maasuuum <fardousalmasum09@gmail.com>
2024-07-11 09:36:21 +0600

    differeerence beetween other language variables type declaration vs js

---
+++ types.js
@@ -0,0 +1,15 @@
+// variable type declaration in strongly typed language 
+// int a = 5;
+// string b = 'alim halim khalim';
+// bool c = True;
+// object student = { name: 'noya daman', id: 55}
+// int[] numbers = [12, 45, 78]
+// string[] friends = ['abul', 'babul'];
+
+// don't need variable type declaration in js/dynamic language 
+const a = 5;
+const b = 'samsu amar fupa';
+const d = true;
+const ages = [45, 65, 48];
+const student = {id: 11904043, class: 15};
+console.log(typeof a,typeof b,typeof d,typeof ages,typeof student)
